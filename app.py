0afe8fb3779aa8ea4f73e484355b6bff4ee31c4e
Paolo Oliveira <paolocmo@gmail.com>
2019-01-10 17:07:07 -0300

    v1.1 - add version

--- app.py
+++ app.py
@@ -3,15 +3,20 @@ from flask import Response
 import time
 
 app = Flask(__name__)
+jR = lambda k: Response(k, mimetype="application/json")
 
 @app.route("/")
 def alpha():
-    return Response('{"status":"OK"}', mimetype="application/json")
+    return jR('{"status":"OK"}')
 
-@app.route("/sleep/<value>")
+@app.route("/delay/<value>")
 def beta(value):
     time.sleep(int(value))
-    return Response('{"status":"ok", "delay": %d}' % int(value), mimetype="application/json")
+    return jR('{"status":"ok", "delay": %d}' % int(value))
+
+@app.route("/version")
+def gamma():
+    return jR('{"version":1.1}')
 
 if __name__ == '__main__':
     app.run(debug=True)
